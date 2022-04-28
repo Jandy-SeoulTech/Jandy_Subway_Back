3cd799c60eb49f9a02af9755f3656dd1423ccad3
sangmyung <enjxz97382@gmail.com>
2022-04-28 22:57:49 +0900

    ResponseDto 호선 운행정보 파라미터 추가

--- src/main/java/zandi/server/dto/SubwayResDto.java
+++ src/main/java/zandi/server/dto/SubwayResDto.java
@@ -2,10 +2,15 @@ package zandi.server.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Getter
-@NoArgsConstructor
+@Setter
 public class SubwayResDto {
 
+    private ArrayList<HashMap<String, String>> routeInfo;
     private String transferStation;
 }
