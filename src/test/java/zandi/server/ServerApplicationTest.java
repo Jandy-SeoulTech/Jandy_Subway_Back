d0a496a1790951bd2acee9be778e6ec31d429c86
DESKTOP-K84G98C\82103 <enjxz97382@gmail.com>
2022-05-09 17:58:43 +0900

    Bean 조회 테스트 구현

---
+++ src/test/java/zandi/server/ServerApplicationTest.java
@@ -0,0 +1,26 @@
+package zandi.server;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ServerApplicationTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition =
+                    ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name=" + beanDefinitionName + " object=" +
+                        bean);
+            }
+        }
+    }
+}
